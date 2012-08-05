70255c27b6e0334450c0e5b20198803ab9b73373
supox <supox@supox-GA-880GM-D2H.(none)>
2012-08-05 20:40:17 +0300

    Added start of static pages and user model.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WateringProject::Application.config.secret_token = '6e9a0b2a00712c4bd76895ef1742d5397aa65f8c9398771ef9ec9129d5d6ee69016ba3a236694b115f92512b94e09ee13013a09a6b34cbe75ff124564e613606'
