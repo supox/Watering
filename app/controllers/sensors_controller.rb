83c9c27e3cba46bc6c2e405da66821217a353ed8
Ilan Ben Hagai <ilan.ben.hagai@gmail.com>
2012-08-19 23:26:09 +0300

    Added json endpoint for sensor reading

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -4,24 +4,39 @@ class SensorsController < ApplicationController
   before_filter :valid_sprinkler
   before_filter :user_can_show_sprinkler, only: [:show]
   before_filter :valid_api_key, only:[:create] # A machine api key validation
-  before_filter :valid_sensor 
-
+  before_filter :valid_sensor
   def new_reading
     @sensor_reading = @sensor.sensor_readings.build
   end
-  
+
   def create_reading
-    @sensor_reading = @sensor.sensor_readings.build(params[:sensor_reading]);
-    
-    if @sensor_reading.save
-      flash[:success] = t(:reading_created)
-      redirect_to action: "new_reading" 
-    else
-      flash[:error] = t(:could_not_create_reading)
-      render action: "new_reading"
+    respond_to do |format|
+      format.json do
+        logger.debug params.to_yaml
+
+        # skip_before_filter :authenticate_user!
+        @sensor_reading = @sensor.sensor_readings.build(params[:sensor_reading]);
+        if @sensor_reading.save
+          render "sensor_reading_ack"
+        else
+          render "sensor_reading_err", :status => :bad_request
+          logger.debug @sensor_reading.errors.to_yaml
+        end
+      end
+
+      format.html do
+        @sensor_reading = @sensor.sensor_readings.build(params[:sensor_reading]);
+        if @sensor_reading.save
+          flash[:success] = t(:reading_created)
+          redirect_to action: "new_reading"
+        else
+          flash[:error] = t(:could_not_create_reading)
+          render action: "new_reading"
+        end
+      end
     end
   end
-  
+
   def show
     days_to_plot = (params[:time_ago] || 7).to_i
     date_to_plot = days_to_plot.days.ago
@@ -31,23 +46,24 @@ class SensorsController < ApplicationController
       date: reading.read_time,
       value:reading.sensor_value.to_i,
       } }
-      
+    logger.debug @data.to_yaml
+    
     respond_to do |format|
       format.html
       format.js
     end
-    
+
   end
 
   def valid_sensor
     @sensor_id = params[:id]
     redirect_to root_path unless @sensor_id
     @sensor = @sprinkler.sensors.find_by_id @sensor_id
-    redirect_to root_path unless @sensor 
+    redirect_to root_path unless @sensor
   end
-    
+
   def valid_api_key
     # redirect_to root_path unless params[:api_key] == Rails.configuration.sprinklers_api_key
   end
-  
+
 end
--- app/models/sensor_reading.rb
+++ app/models/sensor_reading.rb
@@ -1,27 +1,29 @@
 class SensorReading < ActiveRecord::Base
   attr_accessible :sensor_value, :read_time
-  
+
   belongs_to :sensor
 
-  validates :sensor_value, :numericality => {:message => I18n.t(:only_numbers)} 
+  validates :sensor_value, :numericality => {:message => I18n.t(:only_numbers)}
   validate :read_time_is_date
   validates :sensor_id, presence: true
   before_validation :update_time
 
   default_scope order: 'sensor_readings.read_time'
-  
+
   protected
-  
   def read_time_is_date
     errors.add(:read_time, I18n.t(:must_be_a_valid_date)) unless read_time.is_a?(Time)
   end
 
   def update_time
-    if(read_time.is_a?(String))
-      begin
-        read_time = Time.parse(read_time)
-      rescue
+    begin
+      if read_time.is_a?(Integer)
+        self.read_time=Time.at(read_time)
+      elsif(read_time.is_a?(String))
+        self.read_time=Time.parse(read_time)
       end
+    rescue Exception => e
+      logger.debug "Rescue. ex = " + e.message
     end
-  end  
+  end
 end
