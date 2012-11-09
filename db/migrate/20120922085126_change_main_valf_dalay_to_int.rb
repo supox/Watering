e2dae5987d5d8c273f733ca6ad8d80dd4ac41d9e
Ilan Ben Hagai <ilan.ben.hagai@gmail.com>
2012-11-09 19:46:29 +0200

    db problems... :(

--- db/migrate/20120922085126_change_main_valf_dalay_to_int.rb
+++ db/migrate/20120922085126_change_main_valf_dalay_to_int.rb
@@ -1,6 +1,7 @@
 class ChangeMainValfDalayToInt < ActiveRecord::Migration
   def up
-    change_column :sprinklers, :main_valve_delay, :integer, :default => 0
+    remove_column :sprinklers, :main_valve_delay
+    add_column :sprinklers, :main_valve_delay, :integer, :default => 0
   end
 
   def down
