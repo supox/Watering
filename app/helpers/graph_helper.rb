c274d3667706fd34219db36a63601b6cb448c74d
Ilan Ben Hagai <ilan.ben.hagai@gmail.com>
2013-04-26 21:34:20 +0300

    Fixed missing month in locales

--- app/helpers/graph_helper.rb
+++ app/helpers/graph_helper.rb
@@ -7,17 +7,41 @@ module GraphHelper
     values = graph_data.sort{ |x,y| x[:date].to_i <=> y[:date].to_i}.collect{|d| [d[:date].to_i*1000, d[:value]]}
     @chart = LazyHighCharts::HighChart.new('graph') do |f|
       f.options[:title][:text] = graph_title
-      f.options[:chart][:defaultSeriesType] = "area"
-      f.options[:chart][:inverted] = false
-      f.options[:chart][:zoomType] = 'x'
-      f.options[:legend][:layout] = "horizontal"
-      f.options[:legend][:borderWidth] = "0"
+      f.options[:chart] = {:defaultSeriesType =>"area", :inverted => false, :zoomType => 'x', spacingRight:20}
+      f.options[:legend] = {:layout => "horizontal", :borderWidth => "0"}
+      f.options[:plotOptions] = {
+                area: {
+                    fillColor: {
+                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+                        stops: [
+                            [0, "#4572A7"],
+                            [1, "white"]
+                        ]
+                    },
+                    lineWidth: 1,
+                    marker: {
+                        enabled: false
+                    },
+                    shadow: false,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: nil
+                }
+            }
+      
+      f.options[:rangeSelector] = {
+        :buttons => (1..7).collect{|i| {count: i, type: 'days', text:"#{i} day"} },        
+        :selected => 1
+      }      
       f.options[:tooltip][:formatter] = %|
         function() {
           return '<div>'+ Highcharts.dateFormat('%d %b %H:%M',this.x) + '<br>'
           + '<b>' + this.y +'</b></div>';
         }|.js_code
-      f.series(:name => 'Readings', :color => "#b00000", :data => values)
+      f.series(:name => 'Readings', :color => "#0000b0", :data => values)
       f.options[:xAxis] =
       {
           :minTickInterval => 24*3600 * 1000, :type => "datetime", :dateTimeLabelFormats => { day: "%b %e"}, :title => { :text => nil },
